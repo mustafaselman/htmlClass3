18922004bb46c0aa87c1d4482450ff8f4e11b7ee
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-12-13 20:42:39 +0300

    add

---
+++ 23.ders/myScript.js
@@ -0,0 +1,41 @@
+// let text = "Hello world"
+// console.log(text.charAt(0))
+// console.log(text[0])
+// console.log(text.length)
+// console.log(text.charAt(text.length-1))
+// console.log(text[text.length-1])
+// console.log(text.slice(-1))
+
+// // `` back-tics / template literals
+// let text=`
+// bu
+// bir 
+// textdir
+// `
+// let firstName = "Ali"
+// let lastName = "Can"
+// let text2 = `Welcome ${firstName + lastName}, ${lastName}`
+// console.log(text2)
+
+// let x = 100 / "elma"
+// console.log(x)
+// let x2 = 100 / 0
+// console.log(x2)
+
+// let x;
+// console.log(x)
+
+let x = "10"
+let y = 20
+console.log(typeof y.toString())
+console.log(typeof Number(x))
+
+let z = 9.245
+console.log(z.toFixed(1))
+let a = 9
+console.log(a.toFixed(2))
+
+console.log(Number(true))
+console.log(Number(false))
+console.log(typeof Number("    23  ").toString())
+//trimstart-end(çıkarma) //padstart-end(ekleme)
