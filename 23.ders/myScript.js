59478b60722007a0783aceb71bdf75d204509e4b
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-12-13 22:41:38 +0300

    23.ders sonu

--- 23.ders/myScript.js
+++ 23.ders/myScript.js
@@ -67,9 +67,57 @@
 // const cars2 = ["car1","car2","car3"]
 // const allCars = cars.concat(cars2,["car4"])
 // console.log(allCars)
-const meyveler = ["elma","armut","mandalina","çilek","ayva","muz"]
+// const meyveler = ["elma","armut","mandalina","cilek","ayva","muz"]
 // meyveler.splice(1,2,"çilek","ayva","muz")
 // console.log(meyveler)
-let secmeMeyveler = meyveler.slice(-1)
-let secmeMeyveler2 = meyveler.slice(1,4)
-console.log(secmeMeyveler2)
+// let secmeMeyveler = meyveler.slice(-1)
+// let secmeMeyveler2 = meyveler.slice(1,4)
+// console.log(secmeMeyveler2)
+
+//sort (sıralama)
+// console.log(meyveler.sort())
+// console.log(meyveler.reverse())
+// const numbers = [1,100,20,45]
+// console.log(numbers.sort(function(a,b){return a-b}))
+// console.log(numbers.sort(function(a,b){return b-a}))
+
+// const cars = [{type:"volvo", year:2016},{type:"fiat", year:2001},{type:"bmw", year:2010}]
+// console.log(cars)
+// console.log(cars.sort(function(a,b){return a.year-b.year}))
+
+//itaration(döngüler)
+// const numbers = [45,5,6,55,77]
+// numbers.forEach(
+//     function myFunction(value,index,array){
+        
+//         numbers[index] += 1 ; 
+//         console.log(value)
+//         console.log(index)
+//         console.log(array)
+//     }
+// )
+
+// console.log(numbers)
+// function myFunction(value,index,array){
+//     numbers[index] += 1
+//     return value * 2 ;
+// }
+
+// const numbers2 =numbers.map(myFunction)
+// console.log(numbers)
+
+//map bir değer döndürür ve döndürdüğü değer her bir dizi elemanının değeridir. foreach ise eleman sayısınca yapılan döngülerde işlemler yapar
+//map i daha çok dizi değerini alıp başka bir yerde kullanmak için uygularız fakat foreach de ise sadece fonksiyon içinde gerçekleştirilen işlemler vardır.
+
+const numbers = [45,5,6,55,77]
+const over18 = numbers.filter(filterFunc);
+function filterFunc(val,ind,arr){
+    return val > 18
+}
+console.log(over18)
+
+let first = numbers.findIndex(findFunc)
+function findFunc(val,ind,arr){
+    return val < 18
+}
+console.log(first)
